cd3271c69ccffbbfcf3cbb102fb1fb9aa3d2d4d7
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-06 21:16:23 +0530

    updated

--- youtube-clone/src/pages/Home.js
+++ youtube-clone/src/pages/Home.js
@@ -3,15 +3,18 @@ import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import { useAppDispatch, useAppSelector } from '../hooks/useApp'
 import { getHomePageVideos } from '../store/reducers/getHomePageVideos'
+import {useSelector} from 'react-redux';
 
 const Home = () => {
 
   const dispatch = useAppDispatch();
+  // const videos = useAppSelector((state) => state.youtubeApp.videos);
   const videos = useAppSelector((state) => state.youtubeApp.videos);
 
+  console.log(videos);
   useEffect(() => {
     dispatch(getHomePageVideos(false));
-    console.log(videos);
+     console.log(videos);
   },[dispatch])
 
   return (
