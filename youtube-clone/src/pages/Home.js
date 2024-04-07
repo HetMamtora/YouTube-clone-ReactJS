e768396198083ca24f474db94b20cfed10b255ea
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-07 15:31:42 +0530

    updated

--- youtube-clone/src/pages/Home.js
+++ youtube-clone/src/pages/Home.js
@@ -3,24 +3,50 @@ import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import { useAppDispatch, useAppSelector } from '../hooks/useApp'
 import { getHomePageVideos } from '../store/reducers/getHomePageVideos'
-import {useSelector} from 'react-redux';
+import Spinner from '../components/Spinner'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Card from '../components/Card'
 
 const Home = () => {
 
   const dispatch = useAppDispatch();
-  // const videos = useAppSelector((state) => state.youtubeApp.videos);
   const videos = useAppSelector((state) => state.youtubeApp.videos);
 
-  console.log(videos);
+  
   useEffect(() => {
     dispatch(getHomePageVideos(false));
-     console.log(videos);
+    //console.log(videos);
   },[dispatch])
 
+  // console.log(videos);
   return (
-    <div>
+    <div className='max-h-screen overflow-auto'>
+      <div style={{height:"7.5vh"}}>
         <Navbar />
+      </div>
+
+      <div className='flex' style={{height:"92.5vh"}}>
         <Sidebar />
+        {
+        videos.length ? (
+          <InfiniteScroll 
+          dataLength={videos.length} 
+          next={()=> dispatch(getHomePageVideos(true))}
+          hasMore={videos.length<500}
+          loader={<Spinner/>}
+          height={650}
+          >
+              <div className='grid gap-y-14 gap-x-8 grid-cols-4 p-8'>
+                {videos.map((item) => {
+                  return <Card data={item} key={item.videoId}/>
+                })}
+              </div>
+          </InfiniteScroll>
+        ):(
+          <Spinner/>
+        )
+      }
+      </div>
     </div>
   )
 }
